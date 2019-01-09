76141ca4ffa194143dffbf6a60219004996f71d1
sshpa <lvphuong.sshpa@gmail.com>
2019-01-09 14:06:29 +0700

    update version 0.8

---
+++ R/net_build.R
@@ -0,0 +1,160 @@
+network_init <- function() {
+	return(list(nodes = list(), arcs = c()))
+} 
+
+network_nodeExists <- function(dag, name) {
+	if (is.null(dag))
+		return (FALSE)
+		
+	if (is.null(dag$nodes))
+		return (FALSE)
+	
+	for(n in 1:length(dag$nodes))
+	{
+		#message(dag$nodes[[n]]$name)
+		if (tolower(dag$nodes[[n]]$name) == tolower(name))
+			return(TRUE)
+	}
+	
+	return(FALSE)
+} 
+
+network_hasArc <- function(dag, from, to) {
+	if (!network_nodeExists(dag,to))
+		return(FALSE)
+	
+	if (!network_nodeExists(dag,from))
+		return(FALSE)
+
+	if (tolower(from) %in% tolower(dag$nodes[[to]]$parents))
+		return(TRUE)
+
+	return(FALSE)
+}
+
+network_addNode <- function(dag, name, dist = "norm", prior = NULL) {
+	if (is.null(dag) || missing(dag))
+		dag = network_init()
+		
+	if (is.null(dag$nodes))
+		dag$nodes = list()
+	
+	node = list(name=name, dist=dist, prior=prior)
+	dag$nodes[[name]] = node
+	
+	return(dag)
+} 
+
+network_addArc <- function(dag, from, to) {
+	if (!network_nodeExists(dag, from))
+		message(paste0("Error checking node.\n Invalid node '", from, "'."))
+	
+	if (!network_nodeExists(dag, to))
+		message(paste0("Error checking node.\n Invalid node '", to, "'."))
+
+	if (network_hasArc(dag, from, to))
+		return(dag)
+		
+	dag$nodes[[from]]$children = c(dag$nodes[[from]]$children, to)
+	dag$nodes[[to]]$parents = c(dag$nodes[[to]]$parents, from)
+	
+	return(dag)
+} 
+
+network_nodes <- function(dag) {
+	nodes = c()
+	
+	if (is.null(dag))
+		return(nodes)
+		
+	if (is.null(dag$nodes))
+		return(nodes)
+	
+	for(n in 1:length(dag$nodes))
+	{
+		nodes = c(nodes, dag$nodes[[n]]$name)
+	}
+
+	return(nodes)
+} 
+
+network_vl2bn <- function(dag) {
+	require(bnlearn)
+	
+	bnDag = empty.graph(nodes=network_nodes(dag))
+	
+	for(n in 1:length(dag$nodes))
+	{
+		#message(dag$nodes[[n]]$name)
+		if (length(dag$nodes[[n]]$children) > 0)
+		{
+			for(i in 1:length(dag$nodes[[n]]$children))
+			{
+				bnDag = set.arc(bnDag, from = dag$nodes[[n]]$name, to = dag$nodes[[n]]$children[[i]])
+			}
+		}
+	}
+
+	return(bnDag)
+} 
+
+network_bn2vl <- function(dag) {
+	require(bnlearn)
+	
+	vlDag = network_init()
+	
+	for(n in 1:length(dag$nodes))
+	{
+		nodeName <- names(dag$nodes)[n]
+		#message(nodeName)
+		
+		vlDag = network_addNode(vlDag, name = nodeName)
+	}
+
+	for(n in 1:length(dag$nodes))
+	{
+		if (length(dag$nodes[[n]]$children) > 0)
+		{
+			nodeName <- names(dag$nodes)[n]
+			for(i in 1:length(dag$nodes[[n]]$children))
+			{
+				vlDag = network_addArc(vlDag, from = nodeName, to = dag$nodes[[n]]$children[[i]])
+			}
+		}
+	}
+
+	return(vlDag)
+} 
+
+network_save <- function(filename) {
+	nodes <- read.csv(nodefile, stringsAsFactors=F, header = T)
+
+	for(n in 1:length(dag$nodes))
+	{
+		if (length(dag$nodes[[n]]$children) > 0)
+		{
+			nodeName <- names(dag$nodes)[n]
+			for(i in 1:length(dag$nodes[[n]]$children))
+			{
+				vlDag = network_addArc(vlDag, from = nodeName, to = dag$nodes[[n]]$children[[i]])
+			}
+		}
+	}
+	
+	return(vlDag)
+}
+
+network_load <- function(nodefile, graphfile) {
+	nodes <- read.csv(nodefile, stringsAsFactors=F, header = T)
+
+	vlDag = network_init()
+	
+	for(n in 1:length(nodes))
+	{
+		nodeName <- nodes["name"]
+		
+		vlDag = network_addNode(vlDag, name = nodeName)
+	}
+	
+	return(vlDag)
+}
--- README.md
+++ README.md
@@ -10,3 +10,28 @@ Features:
     * Generating JAGS/STAN code for structures of Bayesian networks for sampling and parameter learning
     * Plotting the network graphs and MCMC
     * Compatible with R 2.x or newer version
+
+Here is the [CHANGELOG](https://github.com/sshpa/baysvl/blob/master/CHANGELOG.md)
+
+# Get started
+
+## Install
+
+To get started abd install the latest snapshot type in R console:
+
+```sh
+install.packages("https://github.com/sshpa/baysvl/archive/master.zip")
+```
+
+## Create bayesian network structures
+
+Create network node for each variable in data
+
+```r
+dag <- bayesvl.network_init()
+dag <- bayesvl.network_addNode(dag, "B")
+dag <- bayesvl.network_addNode(dag, "C")
+dag <- bayesvl.network_addNode(dag, "T")
+dag <- bayesvl.network_addNode(dag, "DC")
+dag <- bayesvl.network_addNode(dag, "MD")
+```
