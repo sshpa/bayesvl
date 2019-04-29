0b0def162524601fe2a01797ead00a7d1611157b
sshpa <lvphuong.sshpa@gmail.com>
2019-04-29 12:28:42 +0700

    v0.6.5

---
+++ LectureNotes/2.Model_2b.R
@@ -0,0 +1,43 @@
+data1<-read.csv("/Statistics/1042/1042data/1042data.csv", header = TRUE)
+head(data1)
+
+# Design the model
+model <- bayesvl()
+model <- bvl_addNode(model, "SatIns", "norm")
+model <- bvl_addNode(model, "Res", "cat")
+model <- bvl_addNode(model, "Insured", "cat")
+model <- bvl_addNode(model, "SES", "cat")
+model <- bvl_addNode(model, "End", "cat")
+
+model <- bvl_addArc(model, "Res", "SatIns", "slope")
+model <- bvl_addArc(model, "Insured", "SatIns", "slope")
+model <- bvl_addArc(model, "SES", "SatIns", "slope")
+model <- bvl_addArc(model, "End", "SatIns", "varint")
+
+
+bvl_bnStrength(model, data1[c("Res","SatIns","Insured","SES","End")])
+bvl_bnBayes(model, data1[c("Res","SatIns","Insured","SES","End")])
+bvl_bnBarchart(model, data1[c("Res","SatIns","Insured","SES","End")])
+
+model_string <- bvl_model2Stan(model)
+cat(model_string)
+
+dat1042 <- with(data1,
+            list(Nobs         = length(Res),
+                 NEnd         = length(unique(End)),
+                 End          = as.numeric(End),
+                 NSES         = length(unique(SES)),
+                 SES          = as.numeric(SES),
+                 NRes         = length(unique(Res)),
+                 Res          = as.numeric(Res),
+                 NInsured     = length(unique(Insured)),
+                 Insured      = as.numeric(Insured),
+                 SatIns       = as.numeric(SatIns)))
+
+options(mc.cores = parallel::detectCores())
+
+# Fit the model
+fit <- bvl_modelFit(model, dat1042, warmup = 2000, iter = 5000, chains = 4, cores = 1)
+
+
+
---
+++ LectureNotes/2.Model_2c_failed.R
@@ -0,0 +1,32 @@
+# Design the model
+model <- bayesvl()
+model <- bvl_addNode(model, "Burden", "norm")
+model <- bvl_addNode(model, "Res", "cat")
+model <- bvl_addNode(model, "Insured", "cat")
+
+model <- bvl_addArc(model, "Res", "Burden", "varint")
+model <- bvl_addArc(model, "Insured", "Burden", "varint")
+
+#  Generate the stan code for model
+model_string <- bvl_model2Stan(model)
+cat(model_string)
+
+library(bayesplot)
+
+data1<-read.csv("/Statistics/1042/1042data/1042data.csv", header = TRUE)
+head(data1)
+
+dat1042 <- with(data1,
+            list(Nobs         = length(Res),
+            		 NRes         = length(unique(Res)),
+                 Res          = as.numeric(Res),
+                 NInsured     = length(unique(Insured)),
+                 Insured      = as.numeric(Insured),
+                 Burden     	= as.numeric(Burden)))
+
+options(mc.cores = parallel::detectCores())
+
+# Fit the model
+model <- bvl_modelFit(model, dat1042, warmup = 2000, iter = 5000, chains = 4, cores = 4)
+
+
