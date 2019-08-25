21302069ba9867d27ded82aa60f42fda7db66c39
sshpa <lvphuong.sshpa@gmail.com>
2019-08-25 19:02:34 +0700

    v0.8.5

--- LectureNotes/6.SSHPA/1.Age_data.R
+++ LectureNotes/6.SSHPA/1.Age_data.R
@@ -15,6 +15,7 @@ ggplot(data=dataAge,
        aes(x=age, y=authors, fill=field)) +
        geom_bar(stat="identity", position=position_dodge()) +
        ylab("# Authors") +
+       xlab("Age") +
        scale_fill_discrete(name = "Fields", labels = c("Economics", "Education", "Medical", "Others")) +
        scale_x_discrete(limits=age_labs)
 
@@ -22,6 +23,7 @@ ggplot(data=dataAge,
        aes(x=age, y=authors, color=field)) +
        geom_line() +
        ylab("# Authors") +
+       xlab("Age") +
        scale_color_discrete(name = "Fields", labels = c("Economics", "Education", "Medical", "Others")) +
        scale_x_discrete(limits=age_labs)
 
@@ -48,7 +50,6 @@ ggplot(data=dataAge,
        scale_x_discrete(limits=age_labs)
 
 # do thi so bai trung binh theo do tuoi
-
 dataAge <- read.csv("/Statistics/sshpa/age_avgarticle.csv", header = TRUE)
 head(dataAge)
 
@@ -58,5 +59,20 @@ ggplot(data=dataAge,
        aes(x=age, y=article, colour=field)) +
        geom_line() +
        ylab("# Articles") +
+       xlab("Age") +
        scale_color_discrete(name = "Fields", labels = c("Economics", "Education", "Medical", "Others")) +
        scale_x_discrete(limits=age_labs)
+
+# do thi tuoi trung binh theo nam
+dataAge <- read.csv("/Statistics/sshpa/age_year.csv", header = TRUE)
+head(dataAge)
+
+age_labs = c("<25","25-29","30-34","35-39","40-44","45-49","50-54","55-59",">=60")
+
+ggplot(data=dataAge,
+       aes(x=pubyear, y=avgage, colour=field)) +
+       geom_line() +
+       ylab("Age") +
+       xlab("Year") +
+       scale_color_discrete(name = "Fields", labels = c("Economics", "Education", "Medical", "Others")) +
+       scale_x_continuous(name = "Year", breaks = seq(min(dataAge$pubyear), max(dataAge$pubyear), by = 1))
