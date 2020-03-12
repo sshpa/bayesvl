b42c64695cebfeb887b857dba1777093ca9d90c9
sshpa <lvphuong.sshpa@gmail.com>
2020-03-12 22:09:27 +0700

    v0.9

---
+++ LectureNotes/4.Stem/4.Model_4b (stem).R
@@ -0,0 +1,48 @@
+data1 <- read.csv("/Statistics/STEM/5000Toan.csv", header = TRUE)
+head(data1)
+
+library(tidyr)
+data1 <- data1 %>% drop_na(TimeSoc, TimeSci)
+
+# Design the model
+model <- bayesvl()
+model <- bvl_addNode(model, "APS45ID", "norm")
+model <- bvl_addNode(model, "Sex", "cat")
+model <- bvl_addNode(model, "TimeSci", "cat")
+model <- bvl_addNode(model, "TimeSoc", "cat")
+
+model <- bvl_addArc(model, "TimeSci",  "APS45ID", "slope")
+model <- bvl_addArc(model, "TimeSoc",   "APS45ID", "slope")
+model <- bvl_addArc(model, "Sex", "APS45ID", "varint")
+
+model <- bvl_modelFix(model, data1)
+model_string <- bvl_model2Stan(model)
+cat(model_string)
+
+options(mc.cores = parallel::detectCores())
+
+# Fit the model
+model <- bvl_modelFit(model, data1, warmup = 5000, iter = 10000, chains = 4, cores = 4)
+
+bvl_trace(model)
+
+
+
+# Design the model
+model <- bayesvl()
+model <- bvl_addNode(model, "APS45ID", "norm")
+model <- bvl_addNode(model, "TimeSci", "cat")
+model <- bvl_addNode(model, "TimeSoc", "cat")
+
+model <- bvl_addArc(model, "TimeSci",  "APS45ID", "slope")
+model <- bvl_addArc(model, "TimeSoc",   "APS45ID", "slope")
+
+model <- bvl_modelFix(model, data1)
+model_string <- bvl_model2Stan(model)
+cat(model_string)
+
+options(mc.cores = parallel::detectCores())
+
+# Fit the model
+model <- bvl_modelFit(model, data1, warmup = 2000, iter = 5000, chains = 4, cores = 4)
+
