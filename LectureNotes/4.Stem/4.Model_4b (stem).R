494e17993073fcb78c0d4d50dc99a9bcd54a4af8
sshpa <lvphuong.sshpa@gmail.com>
2020-03-13 16:01:11 +0700

    v0.9

--- LectureNotes/4.Stem/4.Model_4b (stem).R
+++ LectureNotes/4.Stem/4.Model_4b (stem).R
@@ -46,3 +46,79 @@ options(mc.cores = parallel::detectCores())
 # Fit the model
 model <- bvl_modelFit(model, data1, warmup = 2000, iter = 5000, chains = 4, cores = 4)
 
+data1$Gender <- ifelse(as.numeric(data1$Sex) == 1, 1, 0)
+
+# Design the model
+model <- bayesvl()
+model <- bvl_addNode(model, "Gender", "binom")
+model <- bvl_addNode(model, "TimeSci", "cat")
+
+model <- bvl_addArc(model, "TimeSci",  "Gender")
+
+model <- bvl_modelFix(model, data1)
+model_string <- bvl_model2Stan(model)
+cat(model_string)
+
+options(mc.cores = parallel::detectCores())
+
+# Fit the model
+model <- bvl_modelFit(model, data1, warmup = 2000, iter = 5000, chains = 4, cores = 4)
+
+
+
+data1$Gender <- ifelse(as.numeric(data1$Sex) == 1, 1, 0)
+
+# Design the model
+model <- bayesvl()
+model <- bvl_addNode(model, "Gender", "binom")
+model <- bvl_addNode(model, "TimeSoc", "cat")
+
+model <- bvl_addArc(model, "TimeSoc",  "Gender")
+
+model <- bvl_modelFix(model, data1)
+model_string <- bvl_model2Stan(model)
+cat(model_string)
+
+options(mc.cores = parallel::detectCores())
+
+# Fit the model
+model <- bvl_modelFit(model, data1, warmup = 2000, iter = 5000, chains = 4, cores = 4)
+
+
+
+dataList = bvl_modelData(model, data1)
+mstan <- rstan::stan(model_code = model_string, data = dataList,
+	          		warmup=2000 , iter = 5000, chains = 4, refresh=-1)
+
+
+stan_samples <- stan(model_code = model_string, data = dataList)
+
+
+model_string <- "data{
+     // Define variables in data
+     int<lower=1> Nobs;   // Number of observations (an integer)
+     int<lower=0,upper=1> Gender[Nobs];   // outcome variable
+     int NTimeSci;
+     int<lower=1,upper=NTimeSci> TimeSci[Nobs];
+}
+parameters{
+     // Define parameters to estimate
+     real a_Gender;
+     real b_TimeSci_Gender;
+}
+transformed parameters{
+     // Transform parameters
+     real<lower=0,upper=1> theta_Gender[Nobs];
+     for (i in 1:Nobs) {
+        theta_Gender[i] = a_Gender + b_TimeSci_Gender * TimeSci[i];
+     }
+}
+model{
+     // Priors
+     a_Gender ~ normal(0,100);
+     b_TimeSci_Gender ~ normal( 0, 10 );
+
+     // Likelihoods
+     Gender ~ bernoulli(theta_Gender);
+}
+"
